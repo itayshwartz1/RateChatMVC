ad9904996344c61766095c04d2a036b47831203e
itayshwartz1 <itay2007@gmail.com>
2022-05-08 21:01:45 +0300

    Add project files.

---
+++ NewProject1/Controllers/RatesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewProject1.Models;
+
+namespace NewProject1.Controllers
+{
+    public class RatesController : Controller
+    {
+        private static List<Rate> rates = new List<Rate>();
+
+        public RatesController()
+        {
+            rates = new List<Rate>();
+            rates.Add(new Rate() { Id = 1, Score = 2, Name = "yosi", Comment = "Nice", Time = "10:20" });
+            rates.Add(new Rate() { Id = 2, Score = 2, Name = "yosi", Comment = "Nice", Time = "10:20" });
+            rates.Add(new Rate() { Id = 3, Score = 2, Name = "yosi", Comment = "Nice", Time = "10:20" });
+            rates.Add(new Rate() { Id = 4, Score = 2, Name = "yosi", Comment = "Nice", Time = "10:20" });
+        }
+
+        public IActionResult Index()
+        {
+            return View(rates);
+        }
+
+        public IActionResult List()
+        {
+            return View(rates);
+
+        }
+
+        public IActionResult Detailes(int id)
+        {
+            Rate rate = rates.Find(x => x.Id == id); 
+            return View(rate);
+        }
+
+
+
+    }
+}
---
+++ NewProject1/Models/Rate.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewProject1.Models
+{
+    public class Rate
+    {
+        [Required]
+        public int Id { get; set; } 
+
+        public string Name { get; set; }
+
+        [Required]
+        public int Score { get; set; }
+
+        public string Comment { get; set; }
+
+        [Required]
+        public string Time { get; set; }
+
+
+    }
+}
