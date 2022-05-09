1589b4a2eee137bc2c0901afe07fa7e3961962ad
noamTzuber <noamtooby@gmail.com>
2022-05-09 19:35:02 +0300

    add back

--- NewProject1/Models/Rate.cs
+++ NewProject1/Models/Rate.cs
@@ -4,6 +4,10 @@ namespace NewProject1.Models
 {
     public class Rate
     {
+        public Rate()
+        {
+
+        }
 		public Rate(int id, string name, int score, string comment, string time)
 		{
 			this.Id = id;
