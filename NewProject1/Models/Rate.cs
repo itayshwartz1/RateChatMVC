a8ccdc882be937e4668816790f33140f023e7882
noamTzuber <noamtooby@gmail.com>
2022-05-09 21:10:39 +0300

    test

--- NewProject1/Models/Rate.cs
+++ NewProject1/Models/Rate.cs
@@ -4,6 +4,10 @@ namespace NewProject1.Models
 {
     public class Rate
     {
+        public Rate()
+        {
+
+        }
 		public Rate(int id, string name, int score, string comment, string time)
 		{
 			this.Id = id;
@@ -20,6 +24,7 @@ namespace NewProject1.Models
         public string Name { get; set; }
 
         [Required]
+        [Range(1,5)]
         public int Score { get; set; }
 
         public string Comment { get; set; }
